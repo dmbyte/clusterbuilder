eed08ed24a6c96122da7992676b07375f61e1f4b
byte <byte@172.16.253.110>
2017-02-24 18:41:23 -0600

    further work on ses-installerconfig.py

--- ses-installerconfig.py
+++ ses-installerconfig.py
@@ -25,6 +25,11 @@ makex86 = raw_input('Do you wish to deploy X86_64 nodes from this server? (y or 
 if makex86 == "y" or makex86 == "yes":
     if not os.path.exists('/srv/install/x86'):
         os.makedirs('/srv/install/x86/sles12/sp2/cd1')
+    if not os.path.exists('/srv/www/htdocs/SLE-12-SP2-Server-DVD-x86_64-GM-DVD1.iso'):
+        print('The ISO image for x86_64 needs to be located here:/srv/www/htdocs/SLE-12-SP2-Server-DVD-x86_64-GM-DVD1.iso')
+    else:
+        call(["mount", "-o loop /srv/www/htdocs/SLE-12-SP2-Server-DVD-x86_64-GM-DVD1.iso /srv/install/x86/sles12/sp2/cd1")
+        
       
 
 #are ISOs present?  If not, mkdir and mount them
@@ -32,4 +37,25 @@ runsmt = raw_input('Do you need to run SMT Configuration? (y or n)')
 if runsmt == "y" or runsmt == "yes":
     #launch smt wizard
     call(["yast2", "smt-wizard"])
+
+#collect info needed to deploy dhcpd and others
+dodns=raw_input('Do we need to deploy a DNS server for this deployment? (y or n)')
+if dodns == "n":
+    dns1ip=raw_input('Enter DNS Server IP')
+else:
+    #zypper in named
+    #systemctl enable named
+    #write dns files
+
+domainname=raw_input('Enter the domain name for this deployment:')
+defaultgw=raw_input('Enter the default gateway:')
+amIntp=raw_input('Use this server as NTP server')
+if amIntp=="y":
+    import netifaces as ni
+    ni.ifaddresses('eth0')
+    ntpserver = ni.ifaddresses('eth0')[2][0]['addr']
+else:
+    ntpserver=raw_input('Enter the NTP server address')
+
+
 
