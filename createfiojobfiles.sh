fb0359c4ce662d49e32641af8f20af36cd1b018d
byte <byte@172.16.253.110>
2017-06-13 17:26:48 -0500

    fixed iops graphs

--- createfiojobfiles.sh
+++ createfiojobfiles.sh
@@ -10,6 +10,7 @@ let "mylen2=$mylen - 1"
 rw=${i:$mylen2:1}
 bs=${i:0:$mylen2}
 echo [global] >$i.fio
+echo log_avg_msec=1 >$i.fio
 echo ioengine=aio >>$i.fio
 #if you want to use ioengine=rbd, then other options need to be added
 echo filename=/dev/sdb >>$i.fio
