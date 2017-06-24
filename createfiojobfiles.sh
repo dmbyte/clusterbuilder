f8544e0dd611369af696b4080166478118f4d573
byte <byte@172.16.253.110>
2017-06-24 15:31:28 -0500

    added prompts to createjobfiles for device type and name

--- createfiojobfiles.sh
+++ createfiojobfiles.sh
@@ -2,6 +2,10 @@
 # goal is to work with http://louwrentius.com/creating-storage-benchmark-charts-with-fio-and-gnuplot.html
 # the filename line needs to be modified to fit your device/file used for testing.
 # it should be uniform on all nodes
+echo "enter the full path of the device to use"
+read devpath
+echo "Is the device of type (f)ile or (b)lock?"
+read devtype
 
 for i in 4kr 4kw 8kr 8kw 64kr 64kw 1mr 1mw
 do
@@ -14,7 +18,11 @@ echo log_avg_msec=1 >>$i.fio
 echo ramp_time=300 >>$i.fio
 echo ioengine=aio >>$i.fio
 #if you want to use ioengine=rbd, then other options need to be added
-echo filename=/dev/sdb >>$i.fio
+if [ "$devtype" = "f" ]; then
+    echo size=1000M >>$i.fio
+fi
+ 
+echo filename=$devpath >>$i.fio
 #if using a file for filename instead of a device, you must issue size
 #echo size=100M >>$i.fio
 echo numjobs=1 >>$i.fio
